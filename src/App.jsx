3da947080ee75dd37ea244496e3935be0da06d08
Tobias Andersson <tobiaserikandersson@gmail.com>
2024-10-16 11:49:48 +0300

    url env variables added

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CounterPage from './pages/CounterPage';
 import HomePage from './pages/HomePage';
 
+const basename = process.env.NODE_ENV === 'development' ? '/' : '/github-actions-testing';
 
 function App() {
   return (
-    <Router basename="/github-actions-testing">
+    <Router basename={basename}>
       <Routes>
         <Route path='/' element={<HomePage/>}></Route>
         <Route path='/counter' element={<CounterPage/>}></Route>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const baseUrl = process.env.NODE_ENV === 'development' ? '/' : '/github-actions-testing';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/github-actions-testing'
+  base: baseUrl
 })
